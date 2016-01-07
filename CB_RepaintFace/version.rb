e655b10ab18bf790d2742fd02a6ee03008a7ee2c
Clark Bremer <clarkbremer@gmail.com>
2016-01-07 07:28:13 -0600

    Bump Version for 2016

--- CB_RepaintFace/version.rb
+++ CB_RepaintFace/version.rb
@@ -1,5 +1,5 @@
 module CB_PluginInfo
   base_name = File.dirname(__FILE__).split("/").last
-  const_set("#{base_name}_VERSION", "1.1.1")
-  const_set("#{base_name}_DATE", "12/31/2014")
+  const_set("#{base_name}_VERSION", "1.1.2")
+  const_set("#{base_name}_DATE", "1/7/2016")
 end
